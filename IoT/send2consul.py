b304d54966bf6a0c6df032abc7cee836bc2a2d67
KTK <ktk227@gmail.com>
2015-07-28 14:33:33 -0400

    Using hostname as key

--- IoT/send2consul.py
+++ IoT/send2consul.py
@@ -1,21 +1,28 @@
 
 from __future__ import print_function
 import requests
+import socket
 import base64
-import json
+import time
 
+KEY = socket.gethostname()
+URL = "http://10.232.106.230:8500/v1/kv/" + KEY
 
-URL = "http://10.232.106.230:8500/v1/kv/iot"
+data = 'Hello ' + KEY
 
-data = 'Hello'
-
+# Put data on consul server
 put = requests.put(URL, data)
 
+time.sleep(.5)
+
+# Get data from consul server
 get = requests.get(URL)
 
-print(get, get.json(), type(get.json()))
-
+# Output status code and data type
+print(get, type(get.json()))
 d = get.json()[0]
 print(d)
+
+# Decode value data from base64
 print(base64.b64decode(d['Value']))
 
