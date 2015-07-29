3b1b7d03af7d7cc210933f4ca95d6a382eb15d4b
KTK <ktk227@gmail.com>
2015-07-29 09:35:00 -0400

    Some improvements

--- IoT/send2consul.py
+++ IoT/send2consul.py
@@ -1,5 +1,6 @@
 
 from __future__ import print_function
+from pprint import pprint
 import requests
 import socket
 import base64
@@ -8,10 +9,12 @@ import time
 KEY = socket.gethostname()
 URL = "http://10.232.106.230:8500/v1/kv/" + KEY
 
-data = 'Hello ' + KEY
+put_data = 'Hello ' + KEY
 
 # Put data on consul server
-put = requests.put(URL, data)
+put = requests.put(URL, put_data)
+print("Put Data: {}".format(put_data), put)
+print()
 
 time.sleep(.5)
 
@@ -19,10 +22,13 @@ time.sleep(.5)
 get = requests.get(URL)
 
 # Output status code and data type
-print(get, type(get.json()))
+#print(get, type(get.json()))
+print()
 d = get.json()[0]
-print(d)
+pprint(d)
+print()
 
 # Decode value data from base64
-print(base64.b64decode(d['Value']))
+get_data = base64.b64decode(d['Value']).decode()
+print("Get Data: {}".format(get_data), get)
 
