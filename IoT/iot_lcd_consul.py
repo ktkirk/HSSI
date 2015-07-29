45874b6308b39899a221221c754fb0f97e411f52
KTK <ktk227@gmail.com>
2015-07-29 15:34:08 -0400

    iot puts data on consul key/value

---
+++ IoT/iot_lcd_consul.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import pyupm_grove as grove
+import pyupm_i2clcd as lcd
+import pyupm_th02 as th02
+import pyupm_guvas12d as upmUV
+import pyupm_grovemoisture as upmMoisture
+import requests
+import time
+from iot_utils import *
+
+__author__ = 'KT Kirk'
+
+# Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)
+myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+myLcd.setColor(53, 249, 39 ) # Green
+myLcd.setCursor(0,0)
+myLcd.write('IoT')
+
+# Instantiate a Grove Moisture sensor on analog pin A1
+moisture = upmMoisture.GroveMoisture(1)
+
+# Create the light sensor object using AI2 pin 2
+light = grove.GroveLight(2)
+
+# Instantiate a UV sensor on analog pin A3
+uv = upmUV.GUVAS12D(3);
+# analog voltage, usually 3.3 or 5.0
+GUVAS12D_AREF = 5.0;
+SAMPLES_PER_QUERY = 1024;
+
+# Create the temperature sensor object using AIO pin 0
+i2c_th = th02.TH02()
+
+# Edison serial number
+device = open("/factory/serial_number").read().strip('\n')
+
+# Consul key/value
+KEY = "hssi/{}".format(device)
+URL = "http://10.232.106.230:8500/v1/kv/" + KEY
+
+items = ["moisture","humidity","uv","light","temp","time"]
+tlocal = time.localtime()
+
+
+while True:
+    temp = i2c_th.getTemperature()
+    humid = i2c_th.getHumidity()
+    lux_val = light.value()
+    uv_val = uv.value(GUVAS12D_AREF, SAMPLES_PER_QUERY)
+    moisture_val = moisture.value()
+    put_data = [moisture_val, humid, uv_val, lux_val, temp, time.asctime(tlocal)]
+
+    myLcd.setCursor(1, 0)
+    try:
+        for c, v in zip(cols, put_data):
+            requests.put(URL + '/{}'.format(c), v)
+    except requests.exceptions.ConnectionError as e:
+        print("Connection error")
+        myLcd.setColor(255, 0, 0) # Red
+        myLcd.write("Error")
+    else:
+        myLcd.setColor(53, 39, 249) # Blue
+        myLcd.write("{}".format(put))
+
+    time.sleep(60)
--- grovekit/rgb-lcd.py
+++ grovekit/rgb-lcd.py
@@ -21,25 +21,40 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import sys, signal, atexit
 import pyupm_i2clcd as lcd
 import time
 import random
 
+## Exit handlers ##
+# This function stops python from printing a stacktrace when you hit control-C
+def SIGINTHandler(signum, frame):
+    raise SystemExit
+
+# This function lets you run code on exit, including functions from myUVSensor
+def exitHandler():
+    print("Exiting")
+    try:
+        sys.exit(0)
+    except KeyError:
+        pass
+
+# Register exit handlers
+atexit.register(exitHandler)
+signal.signal(signal.SIGINT, SIGINTHandler)
+
 # Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS) 
 myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
-myLcd.setCursor(0,0)
-# RGB Blue
-#myLcd.setColor(53, 39, 249)
-
 while(True):
-    r = random.randint(0, 256)
-    g = random.randint(0, 256)
-    b = random.randint(0, 256)
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
     myLcd.setColor(r, g, b)
 
+    myLcd.setCursor(0,0)
     myLcd.write('Hello World')
-    myLcd.setCursor(1,2)
-    myLcd.write("R: {} G: {} B:{}".format(r, g, b))
+    myLcd.setCursor(1, 0)
+    myLcd.write("R:{} G:{} B:{}".format(r, g, b))
 
     time.sleep(1)
